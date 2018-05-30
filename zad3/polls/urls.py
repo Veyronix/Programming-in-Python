7b7bf4c6fdd4e333e3c55ceedfd6790b9e24b57e
Veyronix <bartekpi11@gmail.com>
2018-05-30 20:59:07 +0200

    Third exercise

--- zad3/polls/urls.py
+++ zad3/polls/urls.py
@@ -3,20 +3,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # ex: /polls/5/TO
+    path('<int:course_id>/name', views.course_detail, name='name'),
     # ex: /polls/
-    path('', views.index, name='index'),
-    # ex: /polls/5/course
-    # path('<int:course_id>/course', views.course_detail, name='course_detail'),
-    path('<int:course_id>/name', views.course_name, name='name'),
-    # ex: /polls/courses
-    path('courses', views.courses, name='courses'),
-    
-    path('<str:name>/add_course',views.add_course,name = 'add_course'),
-
+    path('', views.courses, name='courses'),
+    # ex: /polls/new_course
     path('new_course',views.new_course,name = 'new_course'),
-
+    # ex: /polls/Ruby/new_enrollment
     path('<str:name>/new_enrollment',views.new_enrollment,name = 'new_enrollment')
-
-    # path('signed_up',views.new_enrollment,name = 'new_enrollment')
-
+    
 ]
--- zad3/polls/views.py
+++ zad3/polls/views.py
@@ -4,9 +4,7 @@ from django.template import loader
 from .models import Courses,People,Enrollment
 from .forms import NewCourse, NewEnrollment
 from django.shortcuts import redirect
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+import datetime
 
 def course(request, course_id):
     list_of_people_registered = Courses.objects.filter(id = course_id)
@@ -25,50 +23,33 @@ def courses(request):
     return render(request, 'polls/courses.html', context)
 
 def course_detail(request,course_id):
-    list_of_people_registered = Enrollment.objects.filter(course_id = course_id)
-    template = loader.get_template('polls/course_detail.html')
-    context = {
-        'list_of_people_registered': list_of_people_registered
-    }
-    return render(request, 'polls/course_detail.html', context)
-
-def course_name(request,course_id):
     name = Courses.objects.filter(id = course_id)[0]
     template = loader.get_template('polls/course_detail.html')
     list_of_people_registered = Enrollment.objects.filter(course_id = course_id)
-    # template = loader.get_template('polls/course_detail.html')
     amount_of_vacancies = Courses.objects.get(id = course_id).amount_of_places - Enrollment.objects.filter(course_id = course_id).count()
-    
+    date = Courses.objects.filter(id = course_id)[0].date
     context = {
         'list_of_people_registered': list_of_people_registered,
         'name': name,
-        'amount_of_vacancies': amount_of_vacancies
+        'amount_of_vacancies': amount_of_vacancies,
+        'date': date
     }
-    # return render(request, 'polls/course_detail.html', context)
-    # context = {
-    #     'name': name
-    # }
     return render(request, 'polls/course_detail.html', context)
-
-def add_course(request,name):
-    if Courses.objects.filter(name = name).exists():
-        new_course = "Course exists already."
-    else:
-        new_course = Courses.objects.create(name = name,amount_of_places = 10,date = "2018-12-12 12:30")
-    template = loader.get_template('polls/add_course.html')
-    context = {
-        'new_course': new_course
-    }
-    return render(request, 'polls/add_course.html', context)
 
 def new_course(request):
     if request.method == "POST":
         form = NewCourse(request.POST)
         if form.is_valid():
-            if int(request.POST['amount_of_places']) <= 0:
+            # d=datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d %H:%M")
+            # dnow=datetime.datetime.now()
+            if datetime.datetime.now() > datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d %H:%M"):
+                context = "Too late"
+            elif int(request.POST['amount_of_places']) <= 0:
                 context = "Bad amount of places."
             elif Courses.objects.filter(name=request.POST['name']).exists(): 
                 context = "Course already exists"
+            # elif dnow > d
+            #     context = "Too late"
             else:
                 Courses.objects.create(name = request.POST['name'],amount_of_places = request.POST['amount_of_places'],date = request.POST['date'])
                 context = "Created course"
