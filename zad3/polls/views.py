0483ce4962405c87ff63c09a9c71668daeb86b22
Veyronix <bartekpi11@gmail.com>
2018-06-12 19:37:04 +0200

    Deleted trash

--- zad3/polls/views.py
+++ zad3/polls/views.py
@@ -1,18 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
-from .models import Courses,People,Enrollment
+from .models import Courses, People, Enrollment
 from .forms import NewCourse, NewEnrollment
 from django.shortcuts import redirect
 import datetime
 
+
 def course(request, course_id):
-    list_of_people_registered = Courses.objects.filter(id = course_id)
+    list_of_people_registered = Courses.objects.filter(id=course_id)
     template = loader.get_template('polls/course.html')
     context = {
         'list_of_people_registered': list_of_people_registered
     }
     return render(request, 'polls/course.html', context)
+
 
 def courses(request):
     all_courses = Courses.objects.all
@@ -22,12 +24,14 @@ def courses(request):
     }
     return render(request, 'polls/courses.html', context)
 
-def course_detail(request,course_id):
-    name = Courses.objects.filter(id = course_id)[0]
+
+def course_detail(request, course_id):
+    name = Courses.objects.filter(id=course_id)[0]
     template = loader.get_template('polls/course_detail.html')
-    list_of_people_registered = Enrollment.objects.filter(course_id = course_id)
-    amount_of_vacancies = Courses.objects.get(id = course_id).amount_of_places - Enrollment.objects.filter(course_id = course_id).count()
-    date = Courses.objects.filter(id = course_id)[0].date
+    list_of_people_registered = Enrollment.objects.filter(course_id=course_id)
+    amount_of_vacancies = Courses.objects.get(id=course_id).amount_of_places - Enrollment.objects.filter(
+        course_id=course_id).count()
+    date = Courses.objects.filter(id=course_id)[0].date
     context = {
         'list_of_people_registered': list_of_people_registered,
         'name': name,
@@ -36,46 +40,41 @@ def course_detail(request,course_id):
     }
     return render(request, 'polls/course_detail.html', context)
 
+
 def new_course(request):
     if request.method == "POST":
         form = NewCourse(request.POST)
         if form.is_valid():
-            # d=datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d %H:%M")
-            # dnow=datetime.datetime.now()
             try:
-                # d = datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d %H:%M")
-            
                 if datetime.datetime.now() > datetime.datetime.strptime(request.POST['date'], "%Y-%m-%d %H:%M"):
                     context = "Too late"
-                    
                 elif int(request.POST['amount_of_places']) <= 0:
                     context = "Bad amount of places."
-                elif Courses.objects.filter(name=request.POST['name']).exists(): 
+                elif Courses.objects.filter(name=request.POST['name']).exists():
                     context = "Course already exists"
-                # elif dnow > d
-                #     context = "Too late"
                 else:
-                    Courses.objects.create(name = request.POST['name'],amount_of_places = request.POST['amount_of_places'],date = request.POST['date'])
+                    Courses.objects.create(name=request.POST['name'], amount_of_places=request.POST['amount_of_places'],
+                                           date=request.POST['date'])
                     context = "Created course"
 
             except ValueError:
                 context = "Give time of event."
 
-            return render(request,'polls/signed_up.html',{'context': context})
+            return render(request, 'polls/signed_up.html', {'context': context})
     else:
         form = NewCourse()
-    return render(request,'polls/new_course.html',{'form':form})
+    return render(request, 'polls/new_course.html', {'form': form})
 
-def new_enrollment(request,name):
+
+def new_enrollment(request, name):
     if request.method == "POST":
         form = NewEnrollment(request.POST)
         if form.is_valid():
-            course_id = Courses.objects.get(name = name)
-            People.objects.create(name = request.POST['name'],surname = request.POST['surname'])
-            people_pk = People.objects.filter(name = request.POST['name'],surname = request.POST['surname']).first()
-            Enrollment.objects.create(course = course_id,person = people_pk)
-            return render(request,'polls/signed_up.html',{'context': "Signed up"})
+            course_id = Courses.objects.get(name=name)
+            People.objects.create(name=request.POST['name'], surname=request.POST['surname'])
+            people_pk = People.objects.filter(name=request.POST['name'], surname=request.POST['surname']).first()
+            Enrollment.objects.create(course=course_id, person=people_pk)
+            return render(request, 'polls/signed_up.html', {'context': "Signed up"})
     else:
         form = NewEnrollment()
-    return render(request,'polls/new_enrollment.html',{'form':form, 'name':name})
-
+    return render(request, 'polls/new_enrollment.html', {'form': form, 'name': name})
